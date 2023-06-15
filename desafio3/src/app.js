88ae0a71d0c7e459b211b753a4c2d383d53440d5
soadar <60164893+soadar@users.noreply.github.com>
2023-06-15 01:59:54 -0300

    desafio3.2

--- desafio3/src/app.js
+++ desafio3/src/app.js
@@ -3,7 +3,7 @@ import ProductManager from './ProductManager.js';
 
 const app = express();
 
-const manager = new ProductManager('./productos.json');
+const manager = new ProductManager('src/productos.json');
 
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'Bienvenido' })
@@ -12,8 +12,6 @@ app.get('/', (req, res) => {
 app.get('/products', async (req, res) => {
     try {
         const { limit } = req.query;
-        //console.log(req.params);
-        //console.log(req.query);
         if (limit) {
             res.status(200).json(await manager.getProductsLimit(limit))
         } else {
@@ -26,27 +24,18 @@ app.get('/products', async (req, res) => {
 
 app.get('/products/:pid', async (req, res) => {
     try {
-        //console.log(req.params);
         const { pid } = req.params;
-        //console.log(pid);
         const prod = await manager.getProductById(Number(pid));
         if (prod) {
             res.json(prod)
         } else {
-            res.json({ msg: 'Producto no encontrado' })
+            res.json({ message: 'Producto no encontrado' })
         }
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 })
 
-
-//! PRECIO MAYOR A: --- |BUSCAR| ---> GET a /products?value=`${value}`
-
 app.listen(8090, () => {
     console.log('server express listening on port 8090');
 });
-
-// app.listen(0, function(){
-//     console.log(`server express listening on port ${this.address().port}`);
-// });
